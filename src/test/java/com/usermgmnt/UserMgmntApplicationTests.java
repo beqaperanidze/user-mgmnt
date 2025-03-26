41470c2b04a02e039346b251820ffe4031e7bd71
beqaperanidze <beqaperanidze28@gmail.com>
2025-03-26 11:04:11 +0400

    initial commit

---
+++ src/test/java/com/usermgmnt/UserMgmntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usermgmnt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMgmntApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
